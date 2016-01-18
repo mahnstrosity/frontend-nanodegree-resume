fad502259edab5e61432c8e269535235082460f0
mahnstrostiy <markmahn@verizon.net>
2016-01-17 22:14:15 -0500

    resume builder cleanup at end of lesson1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,105 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// var awesomeThoughts = "I am Mahnstrostiy and I am AWESOME!";
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// // console.log(funThoughts);
+// // $("#main").append(funThoughts);
+
+// var name = "Mark S. Mahn";
+// var role = "Mahnstrosity";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+// var skills = ["AngularJS, JavaScript, JQuery, HTML, CSS, Java, C++"];
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+var work = {
+
+	"jobs": [{
+			"employer": "aXSeum Solutions",
+			"title": "Senior Software Engineer",
+			"location": "Fort Eustis, VA",
+			"dates": "2014 - Present",
+			"description": "working on CAR/TAG"
+		},
+
+		{
+			"employer": "Zantech",
+			"title": "Software Programmer",
+			"location": "Fort Eustis, VA",
+			"dates": "2011 - 2014",
+			"description": "working on TDC/CAR/TAG"
+		}
+	]
+}
+
+var projects = {
+	"projects": [{
+			"title": "Speech Simulator",
+			"dates": "2010 - Present",
+			"description": "TBA",
+			"images": [{
+				"url": "n/a"
+			}, {
+				"url": "n/a"
+			}]
+		},
+
+		{
+			"title": "iSa",
+			"dates": "2007 - 2008",
+			"description": "iTouch Speech Assistant",
+			"images": [{
+				"url": "n/a"
+			}, {
+				"url": "n/a"
+			}]
+		}
+	]
+}
+
+var bio = {
+	"name": "Mark S. Mahn",
+	"role": "Mahnstrosity",
+	"contacts" : {
+		"mobile" : "(757)298-6899",
+		"email" : "markmahn@gmail.com",
+		"github" : "mahnstrosity",
+		"twitter" : "n/a",
+		"location" : "Newport News, VA"
+	},
+	
+	"welcomeMessage" : "Welcome to Hell",
+	"skills": ["AngularJS, JavaScript, JQuery, HTML, CSS, Java, C++"],
+	"bioPic" : "http://www.cs.odu.edu/~cpi/old/cpi-s2008/isa/mahn.jpg"
+}
+
+
+var education = {
+	"college" : "Old Dominion Universtiy",
+	"years" : "2007 - 2008"
+}
+
+var education = {
+	"schools" : [
+		{ "name" : "Old Dominion University",
+		"location" : "Norfolk, VA",
+		"degree" : "BSCS",
+		"major" : "Software Engineering",
+		"dates" : "2007 - 2008",
+		"url" : "http://www.odu.edu"},
+		{ "name" : "Christopher Newport University",
+		"location" : "Newport News, VA",
+		"degree" : "BSGS",
+		"major" : "Legal Studies",
+		"dates" : "1990 - 1994",
+		"url" : "http://www.cnu.edu"}
+	]
+}
+
+// $("#main").append("Name: " + bio.name);
+// $("#main").append("<br>Role: " + bio.role);
+// $("#main").append("<br>Contact Info: " + bio.contacts.mobile);
+// $("#main").append("<br>Pic URL: " + bio.bioPic);
+// $("#main").append("<br>" + bio.welcomeMessage);
+// $("#main").append("<br>Skills: " + bio.skills);
+// $("#main").append("<br>Position: " + work.position);
+// $("#main").append("<br>College: " + education.college);
